34875d9bfa4559ca2a8401dbdf72419f9b63a38a
sangwoo <dev.sangwoo@gmail.com>
2020-02-19 23:21:41 +0900

    본문 second commit

--- CurriedFunction.js
+++ CurriedFunction.js
@@ -1,3 +1,8 @@
 //curried function
 //할인율을 받고 그다음에 가격을 받는 합성함수
-const getDiscount = (price, rate) => price * rate;
+//const getDiscount = (price, rate) => price * rate;
+//애로우펑션에서 파라미터 다음에 함수 선언부가 나타남
+//합성함수의 경우 함수 선언부 부분에 다음에 받는 파라미터가 등장
+//내부적으로 접근할수 있는 함수를 선언함(closure선언)
+
+const ddx
